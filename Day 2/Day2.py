6859615823ebabb85f9fc97f8904e6d26304fa24
Shantanu Jaipurkar <150985885+Shantanujpk@users.noreply.github.com>
2025-04-25 18:04:39 -0400

    Add files via upload
    #  Modules : Used to existing code which is written by others. 
    #  2 Types of moules : External, Internal (Built- In)
    #  External: Someting Like exterenal code writtne by someone and that code can be used in your own code.
    #  Internal Module: Modules which are there by default in Python 
    #  Modules saves time and extra efforts
    #  PIP is used to install modules 
    #  Example pip install panda 
    
    #  pip is package manager : whatever you need pip will get it for you from internet or outside
    #  pip install pandas will install pandas  (Note: open terminal if you are using VSCode and enter the command in the terminal, If you are in python terminal it will give you an error) 
    import pandas
    import sklearn # scikit-learn (Ml lab)
    import hashlib # this is built in module so we do not need to pip install it.
    import tensorflow # while tensorflow is extenal module so we need to pip install it. (this will give error because we havent installed it )
    # installed labs usually goes in  C:\Users\<YourUsername>\AppData\Local\Programs\Python\Python<version>\Lib\site-packages  location, you can check if needed 
    
    print ("Checking the indtallations")
    
    # List of Python Buile In Modules https://docs.python.org/3/py-modindex.html

---
+++ Day 2/Day2.py
@@ -0,0 +1,20 @@
+
+#  Modules : Used to existing code which is written by others. 
+#  2 Types of moules : External, Internal (Built- In)
+#  External: Someting Like exterenal code writtne by someone and that code can be used in your own code.
+#  Internal Module: Modules which are there by default in Python 
+#  Modules saves time and extra efforts
+#  PIP is used to install modules 
+#  Example pip install panda 
+
+#  pip is package manager : whatever you need pip will get it for you from internet or outside
+#  pip install pandas will install pandas  (Note: open terminal if you are using VSCode and enter the command in the terminal, If you are in python terminal it will give you an error) 
+import pandas
+import sklearn # scikit-learn (Ml lab)
+import hashlib # this is built in module so we do not need to pip install it.
+import tensorflow # while tensorflow is extenal module so we need to pip install it. (this will give error because we havent installed it )
+# installed labs usually goes in  C:\Users\<YourUsername>\AppData\Local\Programs\Python\Python<version>\Lib\site-packages  location, you can check if needed 
+
+print ("Checking the indtallations")
+
+# List of Python Buile In Modules https://docs.python.org/3/py-modindex.html
