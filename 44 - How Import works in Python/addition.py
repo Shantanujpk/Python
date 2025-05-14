f46746d4a80519003648fd6115e4bf4613fbf084
Shantanu Jaipurkar <150985885+Shantanujpk@users.noreply.github.com>
2025-05-14 19:37:11 -0400

    Add files via upload
    Import Statement Working

---
+++ 44 - How Import works in Python/addition.py
@@ -0,0 +1,7 @@
+
+def addition (a,b):
+    print ("Addition function from addition.py file has been called here !!!")
+    result = a+b
+    return result
+
+message = "Addition File import works !!"
