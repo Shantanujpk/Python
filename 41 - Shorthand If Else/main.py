2d11dafc1b4d835baef052dd71a2b7ce2d654100
Shantanu Jaipurkar <150985885+Shantanujpk@users.noreply.github.com>
2025-05-13 23:50:28 -0400

    Add files via upload
    Shorthand If Else Statement

---
+++ 41 - Shorthand If Else/main.py
@@ -0,0 +1,15 @@
+# Short hand if else statement are useful when you want to assign a value to variable based on if else condition 
+# easy to use and understand 
+
+
+a = 10
+b = 11
+
+c = a if a > b else b if a == b else "Noo"
+print(c)
+
+"""If a > b, c = a
+
+Else if a == b, c = b
+
+Else, c = "Noo"""
